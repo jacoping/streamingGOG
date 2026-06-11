ead368db2e2d159aa8e3920a47c7635ffabbaf1d
jacoping <jacoping@gmail.com>
2026-06-11 21:03:07 +0200

    add video name tag

--- settings.js
+++ settings.js
@@ -2,10 +2,13 @@
 // SETTINGS
 // ============================================================
 
+// Show debug info overlay (e.g. filename tag in play.html)
+var DEBUG = true;
+
 // Soglia redirect: dopo ogni video viene generato un numero
 // random tra 0 e 1. Se è maggiore di questo valore, la pagina
 // reindirizza su index.html. (0 = sempre, 1 = mai)
 var REDIRECT_THRESHOLD = 0.4;
 
 // Secondi prima che index.html ritorni su videos/play.html
-var REDIRECT_SECONDS = 2;
+var REDIRECT_SECONDS = 8;
