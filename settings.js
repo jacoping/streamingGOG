addbe1f67597056cafae7752c4279299baf867f4
jacoping <jacoping@gmail.com>
2026-06-12 12:18:27 +0200

    Refactor code structure for improved readability and maintainability

--- settings.js
+++ settings.js
@@ -8,10 +8,12 @@ var DEBUG = true;
 // Soglia redirect: dopo ogni video viene generato un numero
 // random tra 0 e 1. Se è maggiore di questo valore, la pagina
 // reindirizza su index.html. (0 = sempre, 1 = mai)
-var REDIRECT_THRESHOLD = 0.4;
+var REDIRECT_THRESHOLD = 0.8;
 
 // Secondi prima che index.html ritorni su videos/play.html
-var REDIRECT_SECONDS = 8;
+// Il tempo effettivo è scelto a caso tra MIN e MAX ad ogni caricamento
+var REDIRECT_MIN = 6;
+var REDIRECT_MAX = 18;
 
 // Secondi di inattività prima che play.html si auto-refreshi
 var TIMEOUT_SECONDS = 60;
